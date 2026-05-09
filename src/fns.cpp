10d54f926b724b0b5de296b842b3c119a16f0bf5
Fraser Lewis <fraser.iain.lewis@gmail.com>
2026-05-09 19:44:55 +0800

    add some rcpp

---
+++ src/fns.cpp
@@ -0,0 +1,40 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::plugins(cpp14)]]
+
+//' Multivariate Normal Density (RcppArmadillo)
+//'
+//' @param x   Matrix of integration variables (BATCH x N)
+//' @param mu  Mean vector (length N)
+//' @param cov Covariance matrix (N x N)
+//' @param a   Scaling factor
+//' @return Numeric vector of density values (length BATCH)
+//' @export
+// [[Rcpp::export]]
+arma::vec dmvnorm_arma(const arma::mat& x,
+                        const arma::vec& mu,
+                        const arma::mat& cov,double a) {
+   //const int BATCH = x.n_rows;
+   const int N = x.n_cols;
+
+   // Cholesky decomposition: sigma = U^T * U, U upper triangular
+   arma::mat U = arma::chol(cov);
+
+   // log|sigma| = 2 * sum(log(diag(U)))
+   double log_det = 2.0 * arma::sum(arma::log(U.diag()));
+
+   // Log normalising constant
+   double constant = -0.5 * N * std::log(2.0 * M_PI) - 0.5 * log_det;
+
+   // Centre observations: M x N
+   arma::mat x_centered = x.each_row() - mu.t();
+
+   // Solve U^T * z = x_centered^T  =>  z is N x M
+   // Mahalanobis distance = ||z||^2 per column
+   arma::mat z = arma::solve(arma::trimatl(U.t()), x_centered.t());
+
+   arma::vec mahal = arma::sum(arma::square(z), 0).t();
+
+   return arma::exp(constant - 0.5 * mahal)*a;
+ }
+
